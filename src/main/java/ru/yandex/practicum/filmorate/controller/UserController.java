61fc6f8867b92bb7d2311cb48459be1d7d63f31c
Vladislav <VladikaSITX@yandex.ru>
2023-10-07 01:07:49 +0300

    9.0

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final static Logger log = LoggerFactory.getLogger(UserController.class);
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
     private HashMap<Integer, User> users = new HashMap<>();
 
     public HashMap<Integer, User> getUsers() {
@@ -24,20 +24,20 @@ public class UserController {
     }
 
     private int userId;
-    private void setUserId(User user){
-        if(user.getId() <= 0) {
+
+    private void setUserId(User user) {
+        if (user.getId() <= 0)
             user.setId(++userId);
-        }
     }
 
     @GetMapping
-    public List<User> findAll(){
+    public List<User> findAll() {
         log.info("Количество пользователей: {}", users.size());
         return new ArrayList<User>(users.values());
     }
 
     @PostMapping
-    public User create(@Valid @RequestBody User user){
+    public User create(@Valid @RequestBody User user) {
         log.info("Добавление пользователя с id: {}", user.getId());
         validationLogin(user);
         validationName(user);
@@ -48,30 +48,33 @@ public class UserController {
     }
 
     @PutMapping
-    public User update(@Valid @RequestBody User user){
+    public User update(@Valid @RequestBody User user) {
         log.info("Обновление пользователя с id: {}", user.getId());
         validationLogin(user);
         validationName(user);
         validationBirthday(user);
-        if(users.containsKey(user.getId())){
-            users.put(user.getId(),user);
+        if (users.containsKey(user.getId())) {
+            users.put(user.getId(), user);
             return user;
-        }else{
+        } else
             throw new NotFoundException("Нет пользователя с id: " + user.getId());
-        }
+
     }
+
     public void validationLogin(User user) {
-        if (user.getLogin().isBlank() || user.getLogin() == null ) {
+        if (user.getLogin().isBlank() || user.getLogin() == null) {
             throw new ValidationException("Логин не может быть пустым и содержать пробелы.");
         }
     }
+
     public void validationName(User user) {
         if (user.getName() == null || user.getName().isBlank() || user.getName().isEmpty()) {
             user.setName(user.getLogin());
         }
     }
+
     public void validationBirthday(User user) {
-        if(user.getBirthday().isAfter(LocalDate.now()) || user.getBirthday() == null){
+        if (user.getBirthday().isAfter(LocalDate.now()) || user.getBirthday() == null) {
             throw new ValidationException("Дата рождения не может быть в будущем.");
         }
     }
