06652f26b848c609cf439df230d34d676ef76f49
Vladislav <VladikaSITX@yandex.ru>
2023-10-07 00:59:11 +0300

    6.0

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RequestMapping("/films")
 public class FilmController {
     private final Logger log = LoggerFactory.getLogger(FilmController.class);
-    private final int MAX_LENGTH_DESCRIPTION = 200;
+    private final int maxLengthDescription = 200;
     private static final LocalDate MIN_RELEASE_DATE = LocalDate.of(1895, 12, 28);
     private HashMap<Integer, Film> films = new HashMap<>();
 
@@ -26,9 +26,8 @@ public class FilmController {
     int filmId;
 
     private void setFilmId(Film film) {
-        if(film.getId() <= 0)
+        if (film.getId() <= 0)
             film.setId(++filmId);
-
     }
 
     @GetMapping
@@ -65,21 +64,25 @@ public class FilmController {
         }
     }
     public void validationName(Film film) {
+
         if (film == null || film.getName() == null || film.getName().isBlank() || film.getName().isEmpty()
                 || film.getName().length() == 0) {
             throw new ValidationException("Название фильма не может быть пустым.");
         }
     }
     public void validationDescription(Film film) {
-        if (film.getDescription().length() > MAX_LENGTH_DESCRIPTION) {
+
+        if (film.getDescription().length() > maxLengthDescription)
             throw new ValidationException("Максимальная длина описания — 200 символов.");
-        }
+
     }
     public void validationReleaseDate(Film film) {
+
         if (film.getReleaseDate().isBefore(MIN_RELEASE_DATE))
             throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года.");
     }
     public void validationDuration(Film film) {
+
         if (film.getDuration() <= 0)
             throw new ValidationException("Продолжительность фильма должна быть положительной.");
 
